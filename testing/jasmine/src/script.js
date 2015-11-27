7cab1e5f370f605dfb3680e907736f402cb3e4f1
Clarine Kirui <Clarine.Kirui@JAMIIBORABANK.CO.KE>
2015-11-27 12:39:30 +0300

    Final Commit version 3

--- testing/jasmine/src/script.js
+++ testing/jasmine/src/script.js
@@ -1,73 +1,68 @@
-$(document).ready(function() {
- 
-  var set2 = $('set_2').val();
+// Invoke the cardinality function on clicking a button
+$(document).ready(function() { 
+    var set2 = $('set_2').val();
 
-  $('#card').on("click", function() {
-
-     var set1 = $('#set_1').val().trim().split(",");
-      
-      var set_1 = [];
+    $('#card').on("click", function() {
+      var set1 = $('#set_1').val().trim().split(","),      
+          set_1 = [];
 
       set1.forEach(function (num) {
         set_1.push(parseInt(num));
       });
 
-      var set_one = new Set(set_1);
-            
+      var set_one = new Set(set_1);            
     $("#newSet").val(set_one.cardinality());
-  });
+    
+});
 
+// Invoke the union function on clicking a button
   $('#union').on("click", function() {
-    var set1 = $('#set_1').val().trim().split(",");
-    var set2 = $('#set_2').val().trim().split(",");
+    var set1 = $('#set_1').val().trim().split(","),
+        set2 = $('#set_2').val().trim().split(","),
+        set_1 = [],
+        set_2 = [];
 
-      var set_1 = [];
-      var set_2 = [];
-
-      set1.forEach(function (num) {
+    set1.forEach(function (num) {
         set_1.push(parseInt(num));
       });
-      set2.forEach(function (num) {
+    set2.forEach(function (num) {
         set_2.push(parseInt(num));
       });
 
-      var setOne = new Set(set_1);
-      var setTwo = new Set(set_2);
+    var setOne = new Set(set_1),
+        result = setOne.union(set_2);
 
-      var result = setOne.union(set_2);
-      console.dir(result.data);
     $("#newSet").val(result.data);
-  });
+   });
 
 });
 
+// Invoke the subset function on clicking a button
 $('#subset').on("click", function() {
-    var set1 = $('#set_1').val().trim().split(",");
-    var set2 = $('#set_2').val().trim().split(",");
+    var set1 = $('#set_1').val().trim().split(","),
+        set2 = $('#set_2').val().trim().split(","),
+        set_1 = [],
+        set_2 = [];
 
-      var set_1 = [];
-      var set_2 = [];
-
-      set1.forEach(function (num) {
+    set1.forEach(function (num) {
         set_1.push(parseInt(num));
       });
-      set2.forEach(function (num) {
+    set2.forEach(function (num) {
         set_2.push(parseInt(num));
       });
 
-      var setOne = new Set(set_1);
-      var setTwo = new Set(set_2);
+    var setOne = new Set(set_1),
+        result = setOne.subset(set_2);
 
-      var result = setOne.subset(set_2);
     $("#newSet").val(result);
-  });
+});
 
+// Invoke the intersect function on clicking a button
 $('#intersect').on("click", function() {
-    var set1 = $('#set_1').val().trim().split(",");
-    var set2 = $('#set_2').val().trim().split(",");
-
-      var set_1 = [];
-      var set_2 = [];
+    var set1 = $('#set_1').val().trim().split(","),
+        set2 = $('#set_2').val().trim().split(","),
+        set_1 = [],
+        set_2 = [];
 
       set1.forEach(function (num) {
         set_1.push(parseInt(num));
@@ -76,19 +71,18 @@ $('#intersect').on("click", function() {
         set_2.push(parseInt(num));
       });
 
-      var setOne = new Set(set_1);
-      var setTwo = new Set(set_2);
+      var setOne = new Set(set_1),
+          result = setOne.intersect(set_2);
 
-      var result = setOne.intersect(set_2);
     $("#newSet").val(result.data);
-  });
+});
 
+// Invoke the complement function on clicking a button
 $('#complement').on("click", function() {
-    var set1 = $('#set_1').val().trim().split(",");
-    var set2 = $('#set_2').val().trim().split(",");
-
-      var set_1 = [];
-      var set_2 = [];
+    var set1 = $('#set_1').val().trim().split(","),
+        set2 = $('#set_2').val().trim().split(","),
+        set_1 = [],
+        set_2 = [];
 
       set1.forEach(function (num) {
         set_1.push(parseInt(num));
@@ -97,36 +91,36 @@ $('#complement').on("click", function() {
         set_2.push(parseInt(num));
       });
 
-      var setOne = new Set(set_1);
-      var setTwo = new Set(set_2);
+      var setOne = new Set(set_1),
+          result = setOne.complement(set_2);
 
-      var result = setOne.complement(set_2);
     $("#newSet").val(result.data);
-  });
+});
 
+// Invoke the cartesianProduct function on clicking a button
 $('#cartesian').on("click", function() {
-    var set1 = $('#set_1').val().trim().split(",");
-    var set2 = $('#set_2').val().trim().split(",");
-
-	  var set_1 = [];
-	  var set_2 = [];
+    var set1 = $('#set_1').val().trim().split(","),
+        set2 = $('#set_2').val().trim().split(","),
+	      set_1 = [],
+	      set_2 = [];
 
 	  set1.forEach(function (num) {
 	    set_1.push(parseInt(num));
 	  });
-	  set2.forEach(function (num) {
+
+	 set2.forEach(function (num) {
 	    set_2.push(parseInt(num));
 	  });
 
-	  var setOne = new Set(set_1);
-	  var setTwo = new Set(set_2);
+	  var setOne = new Set(set_1),
+	      result = setOne.cartesianProduct(set_2),	
+	      finalString = "";
 
-	  var result = setOne.cartesianProduct(set_2);	
-	  var finalString = "";
 	  for(var i = 0; i < result.length; i++) {
 	  	var temp = result[i].toString();
 	  	finalString += "[ " + temp + " ] ";
 	  }
+
 	  finalString = "[ " + finalString + "]";
     $("#newSet").val(finalString);
-  });
+});
--- testing/jasmine/src/setsOperations.js
+++ testing/jasmine/src/setsOperations.js
@@ -1,33 +1,39 @@
-//Create class Set to manipulate sets
+// Create class Set to manipulate sets
 function Set(array) {
 	this.data = array;
 }
 
-//Get the cardinality length of the array
+// Get the cardinality length of the array
 Set.prototype.cardinality = function() {
 	return this.data.length;
 };
 
-//Funtion to create union of sets
+// Funtion to create union of sets
 Set.prototype.union = function (y) {
 	var obj = {};
 	for (var i = 0; i < this.data.length; i++) { 
-     	obj[this.data[i]] = this.data[i]; // stores each value in x in the object obj
+      // Stores each value in x in the object obj
+     	obj[this.data[i]] = this.data[i]; 
   	}
-  	for (var i = 0; i < y.length;  i++) {
-     	obj[y[i]] = y[i]; // stores each value in y in the object obj
-  	}
-  	var res = []; 
-  	for (var k in obj) {
-    	if (obj.hasOwnProperty(k))  // 
-      		res.push(obj[k]); // pushes every element that returns true to res array
-  	}
-  	var result = new Set(res); // returns the values in res array
 
+  	for (var i = 0; i < y.length;  i++) {
+      // Stores each value in y in the object obj
+     	obj[y[i]] = y[i]; 
+  	  var res = [];
+    } 
+
+  	for (var k in obj) {
+    	if (obj.hasOwnProperty(k)) { 
+          // Pushes every element that returns true to res array
+      		res.push(obj[k]); 
+  	}
+    // Returns the values in res array
+  	var result = new Set(res); 
+   }
     return result;
 };
 
-//Gets common values in two arrays and pushes them to a new array
+// Gets common values in two arrays and pushes them to a new array
 Set.prototype.intersect = function(x) {
     var temp = [];
      for (var i = 0; i < this.data.length; i++) { 
@@ -37,43 +43,51 @@ Set.prototype.intersect = function(x) {
             }
         }
     }
-    var result = new Set(temp);  //appends the values of temp[] as a set
 
-    return result;              //Returns the 
+    // Appends the values of temp[] as a set
+    var result = new Set(temp);  
+    return result;              
 };
 
-//checks if the given set is a subset of another
+// Checks if the given set is a subset of another
 Set.prototype.subset = function(arr) {
     for (var i = 0; i < arr.length; i++) {
         if (this.data.indexOf(arr[i]) === -1) {
-            return false; // returns false if the elements in the second array does not match any element in the first array
+            // Returns false if the elements in the second array does not match any element in the first array
+            return false; 
         }
     }
-    return true;  //Returns true if every element in the second set is in the first set
+
+    // Returns true if every element in the second set is in the first set
+    return true;  
 };
 
 // Returns every element in the first set that is not on the second set 
 Set.prototype.complement = function (y) { 
   var arr = [];
-  for (var i= 0; i < this.data.length; i++) {   
+  for (var i = 0; i < this.data.length; i++) {   
       if (y.indexOf(this.data[i]) === -1 ) {
-        arr.push(this.data[i]); //appends the items from the first array that are not equal to any item in the second array to a new array
+        // Appends the items from the first array that are not equal to any item in the second array to a new array
+        arr.push(this.data[i]); 
       }
   }
-  var result = new Set(arr); // returns the values in res array as a set
 
-    return result;          //returns the result in a set
+  // Returns the values in res array as a set
+  var result = new Set(arr); 
+  // Returns the result in a set
+  return result;          
 };
 
-// Matches every item in one array with every element in another array and 
-//returns a nested array
+// Matches every item in one array with every element in another array and returns a nested array
 Set.prototype.cartesianProduct = function (y) { 
   var arr = [];                                   
-  for (var i= 0; i < this.data.length; i++) { 
-    for (var j = 0; j < y.length; j++) {      
-      arr.push([this.data[i], y[j]]);   //pushes an array of two values from set a and set b into an array
+  for (var i = 0; i < this.data.length; i++) { 
+    for (var j = 0; j < y.length; j++) { 
+      // Pushes an array of two values from set a and set b into an array     
+      arr.push([this.data[i], y[j]]);   
     }
   }
-  
-  return arr;                          //returns the nested array arr;  
-}
+
+  // Returns the nested array arr;
+  return arr;                            
+};
